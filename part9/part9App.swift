851a56ca5b6121449ecd647d06f0837eb24e467f
Nakagawa Kosuke <kosukenakagawa730@gmail.com>
2023-08-16 05:36:37 +0900

    Initial Commit

---
+++ part9/part9App.swift
@@ -0,0 +1,17 @@
+//
+//  part9App.swift
+//  part9
+//
+//  Created by Nakagawa Kosuke on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct part9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
