95edf63d28f34e38c9a9f6be6453172691c0389f
Nakagawa Kosuke <kosukenakagawa730@gmail.com>
2023-08-17 02:43:14 +0900

    firstcommit

--- part9/ContentView.swift
+++ part9/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowAleart = false
+    @State var isShowDialog = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button{
+                isShowAleart = true
+            }label:{
+                Text("アラートを表示")
+            }
+            .alert("タイトル",isPresented: $isShowAleart){
+                Button("キャンセル"){
+                    
+                }
+                Button("OK"){
+                    
+                }
+            }message: {
+                Text("ここに詳細メッセージを書きます")
+            }
+            Button{
+                isShowDialog = true
+            }label:{
+                Text("ダイアログを表示")
+            }
+            .confirmationDialog("タイトル", isPresented:  $isShowDialog, titleVisibility: .visible){
+                Button("選択肢1"){
+                }
+                Button("選択肢2"){
+                }
+                }message: {
+                    Text("メッセージ")
+                }
         }
         .padding()
     }
